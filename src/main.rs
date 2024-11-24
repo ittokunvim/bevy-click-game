22ef1e32deea56ad4e30bde4ea83565c663f9b21
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2024-11-24 21:30:48 +0900

    feat: :bento: Add mainmenu image

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
 
 const GAMETITLE: &str = "クリックゲーム";
 const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
 const BACKGROUND_COLOR: Color = Color::srgb(0.75, 0.75, 0.75);
 const PATH_FONT: &str = "fonts/misaki_gothic.ttf";
 const PATH_SOUND_BGM: &str = "sounds/bgm.ogg";
+const PATH_IMAGE_MAINMENU: &str = "images/mainmenu.png";
 
+const MAINMENU_PADDING: f32 = 50.0;
 const GAMETITLE_FONT_SIZE: f32 = 40.0;
-const GAMETITLE_COLOR: Color = Color::srgb(0.1, 0.1, 0.1);
+const GAMETITLE_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
 const CLICKSTART_TEXT: &str = "クリックしてスタート";
 const CLICKSTART_FONT_SIZE: f32 = 20.0;
-const CLICKSTART_COLOR: Color = Color::srgb(0.2, 0.2, 0.2);
-const CLICKSTART_PADDING: Val = Val::Px(16.0);
+const CLICKSTART_COLOR: Color = Color::srgb(0.8, 0.8, 0.8);
+const BOARD_SIZE: Vec2 = Vec2::new(280.0, 210.0);
+const BOARD_COLOR: Color = Color::srgb(0.2, 0.2, 0.2);
 
 fn main() {
     App::new()
@@ -39,6 +45,8 @@ fn setup_camera(mut commands: Commands) {
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     // Game title
     commands.spawn(
@@ -53,7 +61,7 @@ fn setup(
         .with_style(Style {
             position_type: PositionType::Relative,
             justify_self: JustifySelf::Center,
-            top: Val::Px(WINDOW_SIZE.y / 2.0 - GAMETITLE_FONT_SIZE / 2.0),
+            top: Val::Px(WINDOW_SIZE.y / 2.0 - GAMETITLE_FONT_SIZE / 2.0 - MAINMENU_PADDING),
             ..default()
         }),
     )
@@ -69,13 +77,29 @@ fn setup(
             }
         )
         .with_style(Style {
-            position_type: PositionType::Absolute,
-            right: CLICKSTART_PADDING,
-            bottom: CLICKSTART_PADDING,
+            position_type: PositionType::Relative,
+            justify_self: JustifySelf::Center,
+            top: Val::Px(WINDOW_SIZE.y / 2.0 - GAMETITLE_FONT_SIZE / 2.0 + MAINMENU_PADDING),
             ..default()
         }),
     )
     .insert(Name::new("clickstart"));
+    // Mainmenu Board
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: Mesh2dHandle(meshes.add(Rectangle::new(BOARD_SIZE.x, BOARD_SIZE.y))),
+        material: materials.add(BOARD_COLOR),
+        ..default()
+    })
+    .insert(Name::new("board"));
+    // Background image
+    commands.spawn(
+        SpriteBundle {
+            texture: asset_server.load(PATH_IMAGE_MAINMENU),
+            transform: Transform::from_xyz(0.0, 0.0, -10.0),
+            ..default()
+        },
+    )
+    .insert(Name::new("image"));
     // bgm
     let bgm_sound = asset_server.load(PATH_SOUND_BGM);
 
@@ -84,5 +108,6 @@ fn setup(
             source: bgm_sound,
             settings: PlaybackSettings::LOOP.with_spatial(true),
         }
-    );
+    )
+    .insert(Name::new("bgm"));
  }
