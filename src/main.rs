03f8f3d9c3b5f709d3b08c8b87bf95185a13ac8b
ittokunvim <73953416+ittokunvim@users.noreply.github.com>
2024-11-24 21:43:23 +0900

    fix: :recycle: Add mainmenu.rs

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-use bevy::{
-    prelude::*,
-    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
-};
+use bevy::prelude::*;
+
+mod mainmenu;
 
 const GAMETITLE: &str = "クリックゲーム";
 const WINDOW_SIZE: Vec2 = Vec2::new(640.0, 480.0);
 const BACKGROUND_COLOR: Color = Color::srgb(0.75, 0.75, 0.75);
-const PATH_FONT: &str = "fonts/misaki_gothic.ttf";
-const PATH_SOUND_BGM: &str = "sounds/bgm.ogg";
-const PATH_IMAGE_MAINMENU: &str = "images/mainmenu.png";
 
-const MAINMENU_PADDING: f32 = 50.0;
-const GAMETITLE_FONT_SIZE: f32 = 40.0;
-const GAMETITLE_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
-const CLICKSTART_TEXT: &str = "クリックしてスタート";
-const CLICKSTART_FONT_SIZE: f32 = 20.0;
-const CLICKSTART_COLOR: Color = Color::srgb(0.8, 0.8, 0.8);
-const BOARD_SIZE: Vec2 = Vec2::new(280.0, 210.0);
-const BOARD_COLOR: Color = Color::srgb(0.2, 0.2, 0.2);
+const PATH_FONT: &str = "fonts/misaki_gothic.ttf";
+const PATH_IMAGE_MAINMENU: &str = "images/mainmenu.png";
+const PATH_SOUND_BGM: &str = "sounds/bgm.ogg";
 
 fn main() {
     App::new()
@@ -33,73 +24,17 @@ fn main() {
         )
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(Time::<Fixed>::from_seconds(1.0 / 60.0))
-        .add_systems(Startup, setup_camera)
         .add_systems(Startup, setup)
+        .add_plugins(mainmenu::MainmenuPlugin)
         .run();
-}
-
-fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
 }
 
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    // Game title
-    commands.spawn(
-        TextBundle::from_section(
-            GAMETITLE,
-            TextStyle {
-                font: asset_server.load(PATH_FONT),
-                font_size: GAMETITLE_FONT_SIZE,
-                color: GAMETITLE_COLOR,
-            }
-        )
-        .with_style(Style {
-            position_type: PositionType::Relative,
-            justify_self: JustifySelf::Center,
-            top: Val::Px(WINDOW_SIZE.y / 2.0 - GAMETITLE_FONT_SIZE / 2.0 - MAINMENU_PADDING),
-            ..default()
-        }),
-    )
-    .insert(Name::new("gametitle"));
-    // Click Start
-    commands.spawn(
-        TextBundle::from_section(
-            CLICKSTART_TEXT,
-            TextStyle {
-                font: asset_server.load(PATH_FONT),
-                font_size: CLICKSTART_FONT_SIZE,
-                color: CLICKSTART_COLOR,
-            }
-        )
-        .with_style(Style {
-            position_type: PositionType::Relative,
-            justify_self: JustifySelf::Center,
-            top: Val::Px(WINDOW_SIZE.y / 2.0 - GAMETITLE_FONT_SIZE / 2.0 + MAINMENU_PADDING),
-            ..default()
-        }),
-    )
-    .insert(Name::new("clickstart"));
-    // Mainmenu Board
-    commands.spawn(MaterialMesh2dBundle {
-        mesh: Mesh2dHandle(meshes.add(Rectangle::new(BOARD_SIZE.x, BOARD_SIZE.y))),
-        material: materials.add(BOARD_COLOR),
-        ..default()
-    })
-    .insert(Name::new("board"));
-    // Background image
-    commands.spawn(
-        SpriteBundle {
-            texture: asset_server.load(PATH_IMAGE_MAINMENU),
-            transform: Transform::from_xyz(0.0, 0.0, -10.0),
-            ..default()
-        },
-    )
-    .insert(Name::new("image"));
+    // camera
+    commands.spawn(Camera2dBundle::default());
     // bgm
     let bgm_sound = asset_server.load(PATH_SOUND_BGM);
 
---
+++ src/mainmenu.rs
@@ -0,0 +1,88 @@
+use bevy::{
+    prelude::*,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle},
+};
+
+use crate::{
+    GAMETITLE,
+    WINDOW_SIZE,
+    PATH_FONT,
+    PATH_IMAGE_MAINMENU,
+};
+
+const GAMETITLE_FONT_SIZE: f32 = 32.0;
+const GAMETITLE_COLOR: Color = Color::srgb(0.9, 0.9, 0.9);
+const CLICKSTART_TEXT: &str = "クリックしてスタート";
+const CLICKSTART_FONT_SIZE: f32 = 20.0;
+const CLICKSTART_COLOR: Color = Color::srgb(0.8, 0.8, 0.8);
+const TEXT_PADDING: f32 = 50.0;
+const BOARD_SIZE: Vec2 = Vec2::new(280.0, 210.0);
+const BOARD_COLOR: Color = Color::srgb(0.2, 0.2, 0.2);
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // Game title
+    commands.spawn(
+        TextBundle::from_section(
+            GAMETITLE,
+            TextStyle {
+                font: asset_server.load(PATH_FONT),
+                font_size: GAMETITLE_FONT_SIZE,
+                color: GAMETITLE_COLOR,
+            }
+        )
+        .with_style(Style {
+            position_type: PositionType::Relative,
+            justify_self: JustifySelf::Center,
+            top: Val::Px(WINDOW_SIZE.y / 2.0 - GAMETITLE_FONT_SIZE / 2.0 - TEXT_PADDING),
+            ..default()
+        }),
+    )
+    .insert(Name::new("gametitle"));
+    // Click Start
+    commands.spawn(
+        TextBundle::from_section(
+            CLICKSTART_TEXT,
+            TextStyle {
+                font: asset_server.load(PATH_FONT),
+                font_size: CLICKSTART_FONT_SIZE,
+                color: CLICKSTART_COLOR,
+            }
+        )
+        .with_style(Style {
+            position_type: PositionType::Relative,
+            justify_self: JustifySelf::Center,
+            top: Val::Px(WINDOW_SIZE.y / 2.0 - GAMETITLE_FONT_SIZE / 2.0 + TEXT_PADDING),
+            ..default()
+        }),
+    )
+    .insert(Name::new("clickstart"));
+    // Mainmenu Board
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: Mesh2dHandle(meshes.add(Rectangle::new(BOARD_SIZE.x, BOARD_SIZE.y))),
+        material: materials.add(BOARD_COLOR),
+        ..default()
+    })
+    .insert(Name::new("board"));
+    // Background image
+    commands.spawn(
+        SpriteBundle {
+            texture: asset_server.load(PATH_IMAGE_MAINMENU),
+            transform: Transform::from_xyz(0.0, 0.0, -10.0),
+            ..default()
+        },
+    )
+    .insert(Name::new("image"));
+}
+
+pub struct MainmenuPlugin;
+
+impl Plugin for MainmenuPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Startup, setup);
+    }
+}
